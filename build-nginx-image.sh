f802ab938e29082857bd4366b5b4b158cc2eacc0
Alex Tumwesigye <alex_tumwesigye@ug.jsi.com>
2022-06-21 20:15:57 +0300

    Fix versions

--- build-nginx-image.sh
+++ build-nginx-image.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
  echo "Run the application with nginx"
  if [ -d "./build" ]; then
-    docker build --no-cache -f Dockerfile -t $CI_REGISTRY/hisengine/his_dashboard .
-    docker push $CI_REGISTRY/hisengine/his_dashboard
+    docker build --no-cache -f Dockerfile -t $CI_REGISTRY/hisengine/dash-app .
+    docker push $CI_REGISTRY/hisengine/dash-app
  else
    echo "Build failed"
  fi
