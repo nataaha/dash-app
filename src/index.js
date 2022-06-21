1d143bb3b25d32faac6cc10e2750a7c34f4a6ce2
Alex Tumwesigye <alex_tumwesigye@ug.jsi.com>
2022-06-21 21:54:43 +0300

    Fix ENV Variables

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const initApp = async () => {
       {
         <Admin 
           authProvider={ AuthDhis2Provider }
-          loginPage={<LoginPage/> } 
+          loginPage={<Login3ColsPage/> } 
           registerPage={<SignInPage/> }
           apiConfig = { appData }
           //layout={CustomLayout}
