6b9b99b6e1555c5d430e145cca09b1ca90059eab
Alex Tumwesigye <atumwesigye@gmail.com>
2024-03-09 13:09:11 +0300

    v2.0.2

--- createConfig.py
+++ createConfig.py
@@ -16,6 +16,7 @@ def main():
             'landingPage':os.getenv('ALKUIP_LANDING_PAGE',''),
             'strategy': os.getenv('ALKUIP_STRATEGY','dhis2'),
             'usernameField':os.getenv('ALKUIP_USERNAME_FIELD','username'),
+            'usernameFieldLabel':os.getenv('ALKUIP_USERNAME_FIELD','email address'),
             'credit': os.getenv('ALKUIP_CREDIT',True),
             'footer': os.getenv('ALKUIP_FOOTER',True),
             'signup': os.getenv('ALKUIP_SIGNUP',True),
@@ -24,6 +25,8 @@ def main():
             'creditName':os.getenv('ALKUIP_CREDIT_NAME','Nataaha Hotels'),
             'creditUrl':os.getenv('ALKUIP_CREDIT_URL',''),
             'apps':json.loads(os.getenv('ALKUIP_APPS','[]')),
+            'feedback':json.loads(os.getenv('ALKUIP_FEEDBACK','{}')),
+            'auth':json.loads(os.getenv('ALKUIP_AUTH','{}')),
         }
         json.dump(configData, f, ensure_ascii=False, indent=2)
 
